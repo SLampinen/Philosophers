fcf6cd107f581b69728cb9788263232afadac57b
Sakari Lampinen <109725362+SLampinen@users.noreply.github.com>
2023-07-28 14:57:01 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -6,67 +6,136 @@
 /*   By: slampine <slampine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 15:15:01 by slampine          #+#    #+#             */
-/*   Updated: 2023/07/26 14:48:16 by slampine         ###   ########.fr       */
+/*   Updated: 2023/07/28 14:36:56 by slampine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-unsigned long long ft_abs_time(void)
+unsigned long ft_abs_time(void)
 {
 	struct timeval cur_time;
-	long long s;
-	long u;
+	unsigned long s;
+	unsigned long u;
 	gettimeofday(&cur_time, NULL);
-	s = (unsigned long long)cur_time.tv_sec * 1000;
+	s = cur_time.tv_sec * 1000;
 	u = cur_time.tv_usec / 1000;
-	printf("tv.sec is %lu\n",cur_time.tv_sec);
-	printf("s is %llu, u is %lu, s + u is %llu\n",s,u,s+u);
 	return (s + u);
+}
+unsigned long sim_time(unsigned long begin)
+{
+	struct timeval cur_time;
+	unsigned long s;
+	unsigned long u;
+	gettimeofday(&cur_time, NULL);
+	s = cur_time.tv_sec * 1000;
+	u = cur_time.tv_usec / 1000;
+	return((s + u) - begin);
+}
+void ft_msleep(int time)
+{
+	usleep(time * 1000);
+}
+void ft_eat(t_philo *self)
+{
+	if (self->id % 2)
+	{
+		pthread_mutex_lock(&self->chopstick[self->lchopstick]);
+		printf("%lu philo %i has taken a fork\n",sim_time(self->data->begin),self->id);
+		if (self->rchopstick == self->lchopstick)
+			exit(0) ;
+		pthread_mutex_lock(&self->chopstick[self->rchopstick]);
+		printf("%lu philo %i has taken a fork\n",sim_time(self->data->begin),self->id);
+		printf("%lu philo %i is eating\n",ft_abs_time() - self->data->begin, self->id);
+	}
+	else
+	{
+		pthread_mutex_lock(&self->chopstick[self->rchopstick]);
+		printf("%lu philo %i has taken a fork\n",sim_time(self->data->begin),self->id);
+		if (self->rchopstick == self->lchopstick)
+			exit(0) ;
+		pthread_mutex_lock(&self->chopstick[self->lchopstick]);
+		printf("%lu philo %i has taken a fork\n",sim_time(self->data->begin),self->id);
+		printf("%lu philo %i is eating\n",ft_abs_time() - self->data->begin, self->id);
+	}
+		ft_msleep(self->data->time_eat);
+}
+void ft_finish_eating(t_philo *self)
+{
+	pthread_mutex_unlock(&self->chopstick[self->lchopstick]);
+	pthread_mutex_unlock(&self->chopstick[self->rchopstick]);
+	printf("%lu philo %i is sleeping\n",sim_time(self->data->begin),self->id);
+	self->last_ate = ft_abs_time();
+	ft_msleep(self->data->time_sleep);
+	printf("%lu philo %i is thinking\n",sim_time(self->data->begin),self->id);
 }
 void *routine(void *data)
 {
 	t_philo *self;
 	self = (t_philo *)data;
-	int i = 3;
-	while (i)
+	if (self->id % 2)
 	{
-		printf("My id is %i\n",self->id);
-		printf("Works\n");
-		printf("Last ate at %lu",self->last_ate);
-		i--;
+		printf("%lu philo %i is thinking\n",sim_time(self->data->begin),self->id);
+		ft_msleep(self->data->time_eat + 1);
 	}
-	return(0);
+	while (1)
+	{
+		if (ft_abs_time() > self->last_ate + self->data->time_die && self->died == 0)
+		{
+			printf("%lu philo %i has died, last ate at %lu\n",sim_time(self->data->begin),self->id, ft_abs_time() - self->last_ate);
+			self->died = 1;
+		}
+		if (ft_abs_time() > self->last_ate && self->died == 0)
+		{
+			ft_eat(self);
+			if (self->meals_to_eat > 0)
+				self->meals_to_eat--;
+			ft_finish_eating(self);
+		}
+	}
 }
 void init_data(t_data *data, char **argv)
 {
 	data->begin = ft_abs_time();
-	data->num_of_pihlos = atoi(argv[1]);
+	data->num_of_philos = atoi(argv[1]);
 	data->time_die = atoi(argv[2]);
 	data->time_eat = atoi(argv[3]);
 	data->time_sleep = atoi(argv[4]);
 }
 int init_philo(t_philo **philo, t_data *data)
 {
-	int i;
+	unsigned int i;
+	pthread_mutex_t *chopstick;
+	
 	i = 0;
-	while (i < data->num_of_pihlos)
+	chopstick = malloc(sizeof(pthread_mutex_t) * data->num_of_philos);
+	if (chopstick == NULL)
+		return (0);
+	while (i < data->num_of_philos)
 	{
-		printf("Philo is at address %p\n",philo);
+		pthread_mutex_init(&chopstick[i],NULL);
+		i++;
+	}
+	i = 0;
+	while (i < data->num_of_philos)
+	{
+		//printf("Philo is at address %p\n",philo);
 		philo[i] = malloc(sizeof(t_philo));
 		philo[i]->id = i + 1;
 		philo[i]->last_ate = data->begin;
 		philo[i]->meals_to_eat = -1;
-		philo[i]->lfork = i + 1;
-		philo[i]->rfork = i + 2;
-		if (i + 2 > data->num_of_pihlos)
-			philo[i]->rfork = 1;
+		philo[i]->lchopstick = i + 1;
+		philo[i]->rchopstick = i + 2;
+		if (i + 2 > data->num_of_philos)
+			philo[i]->rchopstick = 1;
+		philo[i]->died = 0;
+		philo[i]->chopstick = chopstick;
 		philo[i]->data = data;
-		printf("Philo %i was created at %p, ate last at %llu\n",philo[i]->id, philo[i], philo[i]->last_ate);
-		printf("Has access to forks lfork %i and rfork %i\n",philo[i]->lfork,philo[i]->rfork);
+		//printf("Philo %i was created at %p, ate last at %lu\n",philo[i]->id, philo[i], philo[i]->last_ate);
+		printf("Has access to forks lfork %i and rfork %i\n",philo[i]->lchopstick,philo[i]->rchopstick);
 		i++;
 	}
-	printf("init philo ended, %i\n",philo[0]->id);
+	//printf("init philo ended, %i\n",philo[0]->id);
 	return (1);
 }
 
@@ -76,27 +145,26 @@ int ft_init(t_data **data, t_philo ***philo, char **argv)
 	if (*data == NULL)
 		return (0);
 	init_data(*data, argv);
-	printf("Data is initialised\n");
-	printf("Num is %i, began at %llu\n",(*data)->num_of_pihlos,(*data)->begin);
-	*philo = (t_philo **)malloc(sizeof(*philo) * (*data)->num_of_pihlos);
+	//printf("Data is initialised\n");
+	//printf("Num is %i, began at %lu\n",(*data)->num_of_philos,(*data)->begin);
+	printf("%i philos exists, need to eat every %i ms, will eat %i ms, then sleep %ims\n",(*data)->num_of_philos, (*data)->time_die, (*data)->time_eat, (*data)->time_sleep);
+	*philo = (t_philo **)malloc(sizeof(philo) * (*data)->num_of_philos);
 	init_philo(*philo, *data);
-	printf("Init_philo worked %i\n",(*philo)[0]->id);
+	//printf("Init_philo worked\n");
 	return (1);
 
 }
 
 void start_philos(t_data *data, t_philo **philo)
 {
-	int i;
+	unsigned int i;
 	pthread_t *thr;
 
 	i = 0;
-	thr = malloc(sizeof(pthread_t) * data->num_of_pihlos);
-	printf("Staring philos at philo address %p, %i\n",philo ,data->num_of_pihlos);
-	while (i < data->num_of_pihlos)
+	thr = malloc(sizeof(pthread_t) * data->num_of_philos);
+	while (i < data->num_of_philos)
 	{
-		printf("starting philo num %d\n",philo[i]->id);
-		//pthread_create(thr[i], 0, routine, philo[i]);
+		pthread_create(&thr[i], 0, routine, philo[i]);
 		i++;
 	}
 }
@@ -111,26 +179,33 @@ void set_meals(t_philo **philo, char **argv)
 		i++;
 	}
 }
+int philos_are_alive(t_data *data, t_philo **philo)
+{
+	unsigned int i;
+	
+	i = 0;
+	while (i < data->num_of_philos)
+	{
+		if (philo[i]->died == 1)
+			return (0);
+		i++;
+	}
+	return (1);
+}
 int main(int argc, char **argv)
 {
 	t_data *data;
 	t_philo **philo;
-	int i;
 
 	data = NULL;
 	philo = NULL;
-	i = 0;
 	if (argc == 5)
 		ft_init(&data, &philo, argv);
-	printf("Philo is at address %p\n",philo);
 	if (argc == 6)
 		set_meals(philo, argv);
-	if (argc < 5 )
-		return(0);
 	start_philos(data, philo);
-	while(1)
+	while(philos_are_alive(data, philo))
 	{
-	sleep(1);
-	printf("TIK\n");
 	}
+	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: slampine <slampine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 15:18:15 by slampine          #+#    #+#             */
-/*   Updated: 2023/07/26 14:38:04 by slampine         ###   ########.fr       */
+/*   Updated: 2023/07/28 14:02:11 by slampine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@
 
 typedef struct s_data
 {
-	unsigned long long	begin;
-	int				num_of_pihlos;
+	unsigned long 	begin;
+	unsigned int	num_of_philos;
 	int 			time_die;
 	int 			time_eat;
 	int 			time_sleep;
@@ -30,14 +30,16 @@ typedef struct s_data
 
 typedef struct s_philo
 {
-	int 					id;
-	int 					meals_to_eat;
-	unsigned long	long	last_ate;
-	int						rfork;
-	int						lfork;
-	t_data					*data;
+	int 			id;
+	int 			meals_to_eat;
+	unsigned long	last_ate;
+	int				rchopstick;
+	int				lchopstick;
+	int				died;
+	pthread_mutex_t	*chopstick;
+	t_data			*data;
 } t_philo;
 
-unsigned long long ft_abs_time(void);
+unsigned long ft_abs_time(void);
 
 #endif
